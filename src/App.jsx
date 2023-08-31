76e85431d7b54f04262fdecef29de8350be73ab4
EddyJCuesta <cuesta79@gmail.com>
2023-08-30 21:41:32 -0400

    fetched the api

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css'
+import { Client } from 'podcast-api';
 
 function App() {
   
-
+  const apiKey = import.meta.env.VITE_API_KEY;
+  const client = Client({ apiKey });
+  client.justListen({
+  }).then((response) => {
+    // Get response json data here
+    console.log(response.data);
+  }).catch((error) => {
+    console.log(error)
+  });
   return (
     <>
-      
+      <h1>hello</h1>
     </>
   )
 }
