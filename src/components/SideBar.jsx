b9e34c5cce78b84acf08c6f5997c83c674ddc08b
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2023-09-02 19:44:05 -0400

    removed code

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -14,11 +14,6 @@ const SideBar = ({walkDuration, handleWalkDurationChange}) => {
                     {/* make it so the user cannot put a negative number */}
                     <input type='number' id='walkTime' value={walkDuration} onChange={()=>handleWalkDurationChange}>
                     </input>
-                    <label htmlFor='genre'>Select your genre.</label>
-                    <select id="genre">
-                        <option value="">Please select</option>
-                    </select>
-                    <button className="submitBtn" class="type">Get List</button>
                 </div>
                 <img className="sidebarImg" src={sidebarImg}></img>
             </div>
