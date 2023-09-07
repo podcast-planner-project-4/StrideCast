93f3445ac2e2a2fbf278fdbba7da07307b473029
laurenratcliffe <ratcliffelau@gmail.com>
2023-09-06 20:20:07 -0400

    performed api call

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,56 @@
+import { useState, useEffect } from 'react';
 import './App.css'
-import { Client } from 'podcast-api'
+import { Client } from 'podcast-api';
 
 function App() {
+      const [podcasts, setPodcasts] = useState([]);
 
-  const apiKey = import.meta.env.VITE_API_KEY 
-  const client = Client({ apiKey });
 
-  client.fetchPodcastGenres({
-    
- 
-  }).then((response) => {
-    
-    console.log(response.data);
-  }).catch((error) => {
-    console.log(error)
-  });
+      useEffect(() => {
+        const apiKey = import.meta.env.VITE_API_KEY;
+        const baseUrl = "https://listen-api.listennotes.com/api/v2";
+  
+        const client = Client({ apiKey });
+  
+        const newUrl = new URL(baseUrl);
+        newUrl.pathname = '/search';
+  
+        client.search({
+            q: 'podcast',
+            sort_by_date: 0,
+            type: 'episode',
+            offset: 0,
+            len_min: 10,
+            len_max: 30,
+            genre_ids: '68,82', 
+            published_before: 1580172454000,
+            published_after: 0,
+            only_in: 'title,description',
+            language: 'English',
+            safe_mode: 0,
+            unique_podcasts: 0,
+            page_size: 10,
+        }).then((response) => {
+            const results = response.data.results
+            setPodcasts(results);
+            console.log(results)
+        }).catch((error) => {
+            console.log(error);
+        });
+      }, [])
 
-  return (
-    <>
-      
-    </>
-  )
+    return (
+      <>
+        <label htmlFor="genreChoice">Choose your genre: </label>
+        <Select 
+        onChange={handleChooseGenre}
+        value={selectedGenre}
+        options={dishTypeOptions}> 
+        </Select>
+
+
+      </>
+    )
 }
-
-
 
 export default App
