ae0a5c732af28ac28c77b39783e53107bbe9484f
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2023-09-14 00:43:59 -0400

    updated sign up/login component framework and initialized sign up/login forms

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,7 +1,19 @@
 function LogIn() {
   return (
-    <div>
-      <h1>THIS IS THE LOG IN PAGE</h1>
+    <div className="formPage">
+      <div className="formContainer">
+        <h2>Log in</h2>
+        <form>
+          <input type="email" id="logInEmail" placeholder="Email:" required></input>
+          <div className="passwordContainer">
+            <input type="password" id="signUpPassword" className="signUpPassword" placeholder="Password:"></input>
+            <i class="fa-regular fa-eye-slash faVisible"></i>
+          </div>
+          <button type="submit">Log in</button> 
+        </form>
+        <p>Don't have an account? Sign up here.</p>
+      </div>
+      <p className="homeLink">Return home</p>
     </div>
   );
 }
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,7 +1,23 @@
 function SignUp() {
   return (
-    <div>
-      <h1>THIS IS THE SIGNUP PAGE</h1>
+    <div className="formPage">
+      <div className="formContainer">
+        <h2>Sign up</h2>
+        <form>
+          <input type="email" id="signUpEmail" placeholder="Email:" required></input>
+          <div className="passwordContainer">
+            <input type="password" id="signUpPassword" className="signUpPassword" placeholder="Password:"></input>
+            <i class="fa-regular fa-eye-slash faVisible"></i>
+          </div>
+          <div className="confirmPasswordContainer">
+            <input type="password" id="signUpConfirmPassword" className="signUpConfirmPassword" placeholder="Confirm password:"></input>
+            <i class="fa-regular fa-eye-slash faVisible"></i>
+          </div>
+          <button type="submit">Create an account</button> 
+        </form>
+        <p>Already have an account? Log in.</p>
+      </div>
+      <p className="homeLink">Return home</p>
     </div>
   );
 }
