253f3b47b94fac18a44d9aabeb1decf8f6760cb4
Calvin Young <calvinyoung@Calvins-Mac-mini.local>
2023-09-21 20:23:16 -0400

    pulling

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,11 +1,15 @@
 import strideCastLogo from "../assets/strideCastLogo.png";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 function SignUp() {
   return (
     <div className="formPage">
       <div className="logInLogoContainer">
-      <Link to="/" className="logoLink" style={{ color: 'inherit', textDecoration: 'inherit'}}>
+        <Link
+          to="/"
+          className="logoLink"
+          style={{ color: "inherit", textDecoration: "inherit" }}
+        >
           <div className="logoContainer">
             <div>
               <img className="logo" src={strideCastLogo}></img>
@@ -17,34 +21,47 @@ function SignUp() {
       <div className="formContainer">
         <h2>Sign up</h2>
         <form>
-          <input 
-            type="email" 
-            id="signUpEmail" 
-            placeholder="Email" 
-            required>
-          </input>
+          <input
+            type="email"
+            id="signUpEmail"
+            placeholder="Email"
+            required
+          ></input>
           <div className="passwordContainer">
-            <input 
-              type="password" 
-              id="signUpPassword" 
-              className="signUpPassword" 
-              placeholder="Password">
-            </input>
+            <input
+              type="password"
+              id="signUpPassword"
+              className="signUpPassword"
+              placeholder="Password"
+            ></input>
             <i class="fa-regular fa-eye-slash faVisible"></i>
           </div>
           <div className="confirmPasswordContainer">
-            <input 
-              type="password" 
-              id="signUpConfirmPassword" className="signUpConfirmPassword" 
-              placeholder="Confirm password">
-            </input>
+            <input
+              type="password"
+              id="signUpConfirmPassword"
+              className="signUpConfirmPassword"
+              placeholder="Confirm password"
+            ></input>
             <i class="fa-regular fa-eye-slash faVisible"></i>
           </div>
-          <button type="submit" className="signUpFormBtn">Create an account</button> 
+          <button type="submit" className="signUpFormBtn">
+            Create an account
+          </button>
         </form>
-        <p>Already have an account? <Link to="/login" className="linkToLogIn">Log in</Link>.</p>
+        <p>
+          Already have an account?{" "}
+          <Link to="/login" className="linkToLogIn">
+            Log in
+          </Link>
+          .
+        </p>
       </div>
-      <p className="homeLink"><Link to="/" className="returnHome">Return home</Link></p>
+      <p className="homeLink">
+        <Link to="/" className="returnHome">
+          Return home
+        </Link>
+      </p>
     </div>
   );
 }
