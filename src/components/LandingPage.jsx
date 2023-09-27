20fee57f6d8656261a557110bf78941f89ef5cf6
Calvin Young <calvinyoung@Calvins-Mac-mini.local>
2023-09-26 20:52:30 -0400

    merge

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,13 +1,12 @@
-import StrideCast_purple from '../assets/StrideCast_purple.png'
+import StrideCast_purple from "../assets/StrideCast_purple.png";
 const LandingPage = () => {
-
-    return (
-        <div className="landingPageContainer">
-            <img src={StrideCast_purple} className="landingPageImg"></img>
-            <h1 className='welcome'>Welcome to StrideCast</h1>
-            <p className="info">[PROJECT IN DEVELOPMENT...]</p>
-        </div>
-    )
-}
+  return (
+    <div className="landingPageContainer">
+      <img src={StrideCast_purple} className="landingPageImg"></img>
+      <h1 className="welcome">Welcome to StrideCast</h1>
+      <p className="info">[PROJECT IN DEVELOPMENT...]</p>
+    </div>
+  );
+};
 
 export default LandingPage;
